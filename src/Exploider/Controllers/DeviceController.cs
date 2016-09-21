8c8fbfaeec39ca83877c171ff61861dcdbc69799
Licshee <licshee@yen.ai>
2016-09-21 18:29:16 +0800

    moving "Data" action from HomeController to DeviceController

---
+++ src/Exploider/Controllers/DeviceController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Exploider.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("dev/[action]")]
+    public class DeviceController : Controller
+    {
+        const string TypeTokenCharacterPattern = @"-!#-'*+.\w^`|~"; // !#$%&'*+-.0-9A-Z^_`a-z|~
+        const string TypeTokenPartPattern = "[" + TypeTokenCharacterPattern + "]+";
+        const string TypeTokenPattern = "^" + TypeTokenPartPattern + "/" + TypeTokenPartPattern + "$";
+        public IActionResult Data()
+        {
+            var query = Request.QueryString.Value;
+            if (string.IsNullOrEmpty(query))
+                return BadRequest();
+
+            query = Uri.UnescapeDataString(query);
+
+            var comma = query.IndexOf(',');
+            if (~comma == 0)
+                return BadRequest();
+
+            var type = query.Substring(1, comma - 1);
+            var data = query.Substring(comma + 1);
+
+            var parts = type.Split(';');
+            var partCount = parts.Length;
+
+            var isBase64 = "base64".Equals(parts[partCount - 1].Trim(), StringComparison.OrdinalIgnoreCase);
+            if (isBase64)
+                partCount--;
+
+            var charset = "US-ASCII";
+
+            switch (partCount)
+            {
+                case 1:
+                    type = parts[0].Trim();
+                    if (type.Length == 0)
+                        type = "text/plain";
+                    break;
+                case 2:
+                    charset = parts[1].Trim();
+                    if (!charset.StartsWith("charset=", StringComparison.OrdinalIgnoreCase))
+                        return BadRequest();
+                    charset = charset.Substring(8);
+                    goto case 1;
+                default:
+                    return BadRequest();
+            }
+
+            if (!Regex.IsMatch(type, TypeTokenPattern))
+                return BadRequest();
+
+            if (!isBase64)
+                return Content(query, type, Encoding.GetEncoding(charset));
+            return File(Convert.FromBase64String(data), type + ";charset=" + charset);
+        }
+    }
+}
